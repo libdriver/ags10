c246242b992406cf2bd67f956c7369143acf6ed8
lishifeng <lishifenging@outlook.com>
2025-04-16 22:41:42 +0800

    fix: fix doc errors

--- doc/mainpage/mainpage.h
+++ doc/mainpage/mainpage.h
@@ -1,10 +1,10 @@
 /**
  * @mainpage  LibDriver AGS10
- * @brief     LibDriver AGS10 is a full function driver of AGS10 and it can run in both MCU and Linux platform.
+ * @brief     LibDriver AGS10 is a full-featured driver of AGS10 and it can run in both MCU and Linux platform.
  * @version   1.0.0
  * @author    Shifeng Li
  *
- * <h3>history</h3>
+ * <h3>History</h3>
  * <table>
  * <tr><th>Date        <th>Version  <th>Author      <th>Description
  * <tr><td>2024/10/31  <td>1.0      <td>Shifeng Li  <td>first upload
